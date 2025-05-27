1f18b40f836173f2737cb78cd07940fb436d22b6
praveen <praveen@kavia.ai>
2025-05-27 16:53:06 +0530

    Auto-commit: Changes as of 2025-05-27T11:23:06.633320+00:00

---
+++ tictactoe_duel/src/components/HelloWorld17.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// PUBLIC_INTERFACE
+const HelloWorld17 = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+};
+
+export default HelloWorld17;
---
+++ tictactoe_duel/src/components/HelloWorld18.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// PUBLIC_INTERFACE
+const HelloWorld18 = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+};
+
+export default HelloWorld18;
